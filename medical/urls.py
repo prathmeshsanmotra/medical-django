9ab0e2c50cdcb51664332b5dfb27c8a21b2fbcd3
Prathmesh Sanmotra <prathmeshsanmotra@gmail.com>
2019-03-30 22:41:07 +0530

    medical

---
+++ medical/urls.py
@@ -0,0 +1,31 @@
+"""medical URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from mysite.views import base, donation, appointment,sympt
+from users import views as user_views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$',base,name='base'),
+    url(r'^Donation/',donation,name='donation'),
+    url(r'^Appointment/',appointment,name='appointment'),
+    url(r'SymptomChecker/',sympt,name='sympt'),
+    url(r'^register/',user_views.register, name='register'),
+    url(r'^login/',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    url(r'^logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+]
